7948ae790218d416ad942f753ebcb7b349c960f5
Theo Istrate <theo.istrate@sensat.co.uk>
2023-05-31 13:59:07 +0100

    trial on change

--- src/base/traverseFunctions.js
+++ src/base/traverseFunctions.js
@@ -126,7 +126,7 @@ export function determineFrustumSet( tile, renderer ) {
 	const loadSiblings = renderer.loadSiblings;
 	const lruCache = renderer.lruCache;
 	const stopAtEmptyTiles = renderer.stopAtEmptyTiles;
-	console.log('trial hello')
+	console.log('trial hello 2')
 	resetFrameState( tile, frameCount );
 
 	// Early out if this tile is not within view.
